aa11dd9abcaf6a951a4ae546f574ed2d3e5f04e3
Urvish Prajapati <urvishmeghani111@gmail.com>
2023-02-05 18:10:36 +0530

    chatbot added

--- src/index.js
+++ src/index.js
@@ -12,8 +12,6 @@ const store = createStore(Reducers, compose(applyMiddleware(thunk)));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   </Provider>
 );
