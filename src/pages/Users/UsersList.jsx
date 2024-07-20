189c4a94f214a80304ce82c03c6573c38fe44dc6
Urvish Prajapati <urvishmeghani111@gmail.com>
2024-07-20 20:50:35 +0530

    snipper added for users

--- src/pages/Users/UsersList.jsx
+++ src/pages/Users/UsersList.jsx
@@ -2,20 +2,31 @@ import React from "react";
 import { useSelector } from "react-redux";
 import User from "./User";
 import "./Users.css";
+import { Spin } from "antd";
 
 const UsersList = () => {
   const users = useSelector((state) => state.usersReducer);
   const searchQuery = useSelector((state) => state.searchQuery);
+  const questionsList = useSelector((state) => state.questionsReducer);
 
   const filteredUsers = users.filter((user) =>
     user?.name.toLowerCase().includes(searchQuery)
   );
 
   return (
-    <div className="user-list-container">
-      {filteredUsers.map((user) => (
-        <User user={user} key={user?._id} />
-      ))}
+    <div>
+      {questionsList.data === null && (
+        <div className="spinner-container">
+          <Spin size="large" style={{ fontSize: "50px" }} />
+        </div>
+      )}
+      {questionsList.data !== null && (
+        <div className="user-list-container">
+          {filteredUsers.map((user) => (
+            <User user={user} key={user?._id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
