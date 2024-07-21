659d73caa3c49bfc4bf183bc0c044bcbc809cad3
Urvish Prajapati <urvishmeghani111@gmail.com>
2024-07-21 21:05:54 +0530

    hosted backend to AWS and integrate API into frontend

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,39 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API = axios.create({ baseURL: 'https://stackoverflow-clone-um2p.onrender.com'})
+const API = axios.create({
+  baseURL:
+    "http://stackoverflow-env.eba-wfuxa5tr.ap-south-1.elasticbeanstalk.com/",
+});
 
 API.interceptors.request.use((req) => {
-    if(localStorage.getItem('Profile')){
-        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('Profile')).token}`
-    }
-    return req;
-})
+  if (localStorage.getItem("Profile")) {
+    req.headers.authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("Profile")).token
+    }`;
+  }
+  return req;
+});
 
-export const logIn = (authData) => API.post('/user/login', authData);
-export const signUp = (authData) => API.post('/user/signup', authData);
+export const logIn = (authData) => API.post("/user/login", authData);
+export const signUp = (authData) => API.post("/user/signup", authData);
 
-export const postQuestion = (questionData) => API.post('/questions/Ask', questionData)
-export const getAllQuestions = () => API.get('/questions/get');
-export const deleteQuestion = (id) => API.delete(`/questions/delete/${id}`) 
-export const voteQuestion = (id, value ) => API.patch(`/questions/vote/${id}`, { value })
+export const postQuestion = (questionData) =>
+  API.post("/questions/Ask", questionData);
+export const getAllQuestions = () => API.get("/questions/get");
+export const deleteQuestion = (id) => API.delete(`/questions/delete/${id}`);
+export const voteQuestion = (id, value) =>
+  API.patch(`/questions/vote/${id}`, { value });
 
-export const postAnswer = (id, noOfAnswers, answerBody, userAnswered , userId ) => API.patch(`/answer/post/${id}`, { noOfAnswers, answerBody, userAnswered ,userId })
-export const deleteAnswer = (id, answerId, noOfAnswers ) => API.patch(`/answer/delete/${id}`, { answerId, noOfAnswers})
+export const postAnswer = (id, noOfAnswers, answerBody, userAnswered, userId) =>
+  API.patch(`/answer/post/${id}`, {
+    noOfAnswers,
+    answerBody,
+    userAnswered,
+    userId,
+  });
+export const deleteAnswer = (id, answerId, noOfAnswers) =>
+  API.patch(`/answer/delete/${id}`, { answerId, noOfAnswers });
 
-export const getAllUsers = () => API.get('/user/getAllUsers');
-export const updateProfile = (id, updateData) => API.patch(`/user/update/${id}`, updateData)
+export const getAllUsers = () => API.get("/user/getAllUsers");
+export const updateProfile = (id, updateData) =>
+  API.patch(`/user/update/${id}`, updateData);
