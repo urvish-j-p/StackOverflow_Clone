1db814c0aa1bbaf123aefcf4954d2529ff7fcd15
Urvish Prajapati <urvishmeghani111@gmail.com>
2024-07-27 20:53:03 +0530

    deployment test3

--- src/components/HomeMainbar/HomeMainbar.jsx
+++ src/components/HomeMainbar/HomeMainbar.jsx
@@ -30,7 +30,7 @@ const HomeMainbar = () => {
     <div className="main-bar">
       <div className="main-bar-header">
         {location.pathname === "/" ? (
-          <h1>Top Questions</h1>
+          <h1>Top Questionssss</h1>
         ) : (
           <h1>All Questions</h1>
         )}
