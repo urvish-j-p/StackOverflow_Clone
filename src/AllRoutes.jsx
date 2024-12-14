4fcecb93337bcb4673b9d6f23b4946dad45baff2
Urvish Prajapati <urvishmeghani111@gmail.com>
2024-12-14 12:30:02 +0530

    tags removed

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -5,7 +5,7 @@ import Auth from "./pages/Auth/Auth";
 import Questions from "./pages/Questions/Questions";
 import AskQuestion from "./pages/AskQuestion/AskQuestion";
 import DisplayQuestion from "./pages/Questions/DisplayQuestion";
-import Tags from "./pages/Tags/Tags";
+// import Tags from "./pages/Tags/Tags";
 import Users from "./pages/Users/Users";
 import UserProfile from "./pages/UserProfile/UserProfile";
 
@@ -18,7 +18,7 @@ const AllRoutes = () => {
       <Route path="/Questions" element={<Questions />} />
       <Route path="/AskQuestion" element={<AskQuestion />} />
       <Route path="/Questions/:id" element={<DisplayQuestion />} />
-      <Route path="/Tags" element={<Tags />} />
+      {/* <Route path="/Tags" element={<Tags />} /> */}
       <Route path="/Users" element={<Users />} />
       <Route path="/Users/:id" element={<UserProfile />} />
     </Routes>
